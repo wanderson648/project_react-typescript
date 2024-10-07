c22b929a3c53eaf2c0f611586a6e77bec282dae4
wanderson648 <testepedroca@gmail.com>
2024-10-07 14:02:46 -0300

    implementa checkbox com funcao anonima

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 export function Checkbox({ label }: { label: string }) {
   const [value, setValue] = useState(false);
 
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    setValue(event.currentTarget.checked)
-  }
-
   return (
     <label
       style={{
         padding: "1rem",
-        border: value ? "2px solid #8d2": "2px solid #f70",
+        border: value ? "2px solid #8d2" : "2px solid #f70",
       }}
     >
-      <input type="checkbox" checked={value} onChange={handleChange} />
+      <input
+        type="checkbox"
+        checked={value}
+        onChange={({ currentTarget }) => setValue(currentTarget.checked)}
+      />
       {label}
     </label>
   );
